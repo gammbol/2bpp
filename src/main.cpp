89253a04d81072f8b8313eb45cde6d84b0578806
tryingtothink <gammbol2525@gmail.com>
2026-02-03 21:35:20 +0300

    NOW finished textures

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,9 @@ int main(int argc, char *argv[]) {
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+  // stbi image vertical flip
+  stbi_set_flip_vertically_on_load(true);
+
   GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "2bpp test", nullptr, nullptr);
   if (window == nullptr) {
     std::cerr << "The program couldn't create a window!" << std::endl;
@@ -59,11 +62,12 @@ int main(int argc, char *argv[]) {
   glEnableVertexAttribArray(1);
 
   // creating texture buffer (i think...)
-  unsigned int texture;
-  glGenTextures(1, &texture);
+  unsigned int texture[2];
+  glGenTextures(2, texture);
 
   // binding texture
-  glBindTexture(GL_TEXTURE_2D, texture);
+  glActiveTexture(GL_TEXTURE0);
+  glBindTexture(GL_TEXTURE_2D, texture[0]);
 
   // setting texture parameters
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
@@ -86,10 +90,32 @@ int main(int argc, char *argv[]) {
   // freeing image byte vector
   stbi_image_free(data);
 
+  // creating second texture
+  data = stbi_load("img/awesomeface.png", &width, &height, &nrChannels, 0);
+
+  if (data) {
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, texture[1]);
+
+    // using GL_RGBA because the resource in png format
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  }
+  else {
+    std::cerr << "stbi_load: Failed to load texture!" << std::endl;
+  }
+  stbi_image_free(data);
+
   // shaders
   shaders shd("shaders/vertex.vs", "shaders/fragment.fs");
   shd.compile_shaders();
   shd.link_program();
+
+  // using the shader program in order to bind textures
+  // because it doesnt want to bind it at all if you add another one
+  shd.use_program();
+  shd.setInt("tex0", 0);
+  shd.setInt("tex1", 1);
 
   // setting resize callback
   glfwSetFramebufferSizeCallback(window, frambuffer_size_callback);
@@ -102,7 +128,11 @@ int main(int argc, char *argv[]) {
 
     shd.use_program();
 
-    glBindTexture(GL_TEXTURE_2D, texture);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture[0]);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, texture[1]);
+
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
     
